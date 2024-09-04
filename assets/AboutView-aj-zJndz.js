29c26bba0cccc9588f332ee9ec98b6945da5900a
Alex <46883023+gaahix@users.noreply.github.com>
2024-09-04 20:57:09 +0300

    Add files via upload

---
+++ assets/AboutView-aj-zJndz.js
@@ -0,0 +1,1 @@
+import{d as n,r,c as u,a,b as i,t as l,u as p,o as c}from"./index-eZNaTuUm.js";const d={class:"about"},b=n({__name:"AboutView",setup(f){var t=r(2020);function o(s){t.value++}return(s,e)=>(c(),u("div",d,[e[1]||(e[1]=a("h1",null,"This is an about page",-1)),a("button",{onClick:e[0]||(e[0]=m=>o())}),i(" "+l(p(t)),1)]))}});export{b as default};
